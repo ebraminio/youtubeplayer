5bb75015f94be4a4b596b99c5f144fbcd333e7b5
ebraminio <ebrahim@gnu.org>
2015-02-13 13:30:17 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,136 @@
+'use strict';
+
+var url = require('url');
+var querystring = require('querystring');
+var ytdl = require('ytdl-core');
+
+function readRangeHeader(range, totalLength) {
+  if (!range) { return; }
+  var positions = range.replace('bytes=', '').split('-');
+
+  var start = parseInt(positions[0], 10);
+  var end = parseInt(positions[1], 10);
+
+  var result = {
+    start: isNaN(start) ? 0 : start,
+    end: isNaN(end) ? (totalLength - 1) : end
+  };
+
+  if (!isNaN(start) && isNaN(end)) {
+    result.start = start;
+    result.end = totalLength - 1;
+  }
+
+  if (isNaN(start) && !isNaN(end)) {
+    result.start = totalLength - end;
+    result.end = totalLength - 1;
+  }
+
+  return result;
+}
+
+var videoSizes = {};
+
+function server(req, res) {
+  if (req.method !== 'GET') {
+    res.writeHead(501);
+    res.end();
+    return;
+  }
+  try {
+    var videoId = querystring.parse(url.parse(req.url).query).v;
+    if (videoId) {
+      ytdl.getInfo('http://www.youtube.com/watch?v=' + videoId, {}, function (err, info) {
+        if (err) {
+          res.writeHead(404);
+          res.end('Seems the link you\'ve given is broken..');
+          return;
+        }
+        res.writeHead(302, { 'Location': '/pr/' + info.title.replace(/ /g, '_') + ' ' + videoId + '.webm' });
+        res.end();
+      });
+      return;
+    }
+
+    videoId = (/([a-zA-Z0-9_-]{11})\.webm$/.exec(req.url) || {})[1];
+    if (!videoId) {
+      res.writeHead(404);
+      res.end('Sorry, nothing really interesting is here');
+      return;
+    }
+
+    var options = {
+      quality: 43, //'lowest',
+      filter: function(format) { return format.container === 'webm'; }
+    };
+    ytdl.getInfo('http://www.youtube.com/watch?v=' + videoId, options, function (err, info) {
+      if (err) {
+        res.writeHead(404);
+        res.end('Seems the link you\'ve given is broken.');
+        return;
+      }
+      var sizePromise;
+      if (!videoSizes[videoId]) {
+        sizePromise = new Promise(function (resolve) {
+          ytdl.downloadFromInfo(info, options).on('format', function (format) {
+            videoSizes[videoId] = format.size;
+            resolve(format.size);
+            //if (format.res) { format.res.end(); }
+          });
+        });
+      } else {
+        sizePromise = Promise.resolve(videoSizes[videoId]);
+      }
+
+      sizePromise.then(function (size) {
+        var range = readRangeHeader(req.headers.range, size);
+
+        if (range) {
+          // If the range can't be fulfilled.
+          if (range.start >= size || range.end >= size) {
+            // 416: 'Requested Range Not Satisfiable'
+            res.writeHead(416, {
+              'Content-Range': 'bytes */' + stat.size
+            });
+            res.end('Requested Range Not Satisfiable');
+            return;
+          }
+
+          options.range = range.start + '-' + range.end;
+        }
+
+        ytdl.downloadFromInfo(info, options).on('format', function (format) {
+          if (!range) {
+            res.writeHead(200, {
+              'Content-Length': format.size,
+              'Content-Type': 'video/webm',
+              'Accept-Ranges': 'bytes'
+            });
+          } else {
+            res.writeHead(206, {
+              'Content-Range': 'bytes ' + range.start + '-' + range.end + '/' + size,
+              'Content-Length': range.start === range.end ? 0 : (range.end - range.start + 1),
+              'Content-Type': 'video/webm',
+              'Accept-Ranges': 'bytes',
+              'Cache-Control': 'no-cache'
+            });
+          }
+        }).pipe(res);
+      }).then(undefined, function (error) {
+        res.writeHead(404);
+        res.end('Internal error ' + error);
+      });
+    });
+  } catch (e) {
+    res.writeHead(404);
+    res.end('Internal error' + e);
+  }
+}
+
+if (process.env.FCGI_MODE) { // called through fcgi
+  require('node-fastcgi').createServer(server).listen();
+} else { // called directly, development
+  var port = process.argv[2] || 19876;
+  require('http').createServer(server).listen(port, '0.0.0.0');
+  console.log('Server running at http://0.0.0.0:' + port + '/');
+}
