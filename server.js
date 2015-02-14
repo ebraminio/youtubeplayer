eeccd5d997ba704108282882f658c86dedd1f27f
ebraminio <ebrahim@gnu.org>
2015-02-14 21:38:01 +0000

    Support itag

--- server.js
+++ server.js
@@ -37,12 +37,14 @@ function server(req, res) {
     res.end();
     return;
   }
-  var videoId = querystring.parse(url.parse(req.url).query).v;
+  var query = querystring.parse(url.parse(req.url).query);
+  var videoId = query.v;
+  var itag = +query.itag || 43;
   if (videoId) {
     ytdl.getInfo('http://youtu.be/' + videoId, {}, function (err, info) {
       if (err) {
         res.writeHead(404);
-        res.end('Seems the link you\'ve given is broken..');
+        res.end('Seems the link you\'ve given is broken');
         return;
       }
       res.writeHead(302, { 'Location': '/pr/' + info.title.replace(/ /g, '_') + ' ' + videoId + '.webm' });
@@ -51,40 +53,49 @@ function server(req, res) {
     return;
   }
 
-  videoId = (/([a-zA-Z0-9_-]{11})\.webm$/.exec(req.url) || {})[1];
+  videoId = (/([a-zA-Z0-9_-]{11})\.(webm|mp4)($|\?)/.exec(req.url) || {})[1];
   if (!videoId) {
     res.writeHead(404);
     res.end('Sorry, nothing really interesting is here');
     return;
   }
 
-  var options = {
-    quality: 43, //'lowest',
-    filter: function(format) { return format.container === 'webm'; }
-  };
-  ytdl.getInfo('http://www.youtube.com/watch?v=' + videoId, options, function (err, info) {
+  ytdl.getInfo('http://youtu.be/' + videoId, {}, function (err, info) {
     if (err) {
       res.writeHead(404);
-      res.end('Seems the link you\'ve given is broken.');
+      res.end('Seems the link you\'ve given is broken');
+      return;
+    }
+    if (req.url.endsWith('?info')) {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end(JSON.stringify(info, 2, 2));
+      return;
+    }
+    if (req.url.endsWith('?list')) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end('<form method="GET"><select name="itag">' + info.formats.map(function (x) {
+        return `<option value="${x.itag}">${x.container} ${x.quality || x.size || 'audio'}</option>`;
+      }).join('\n') + '</select><input type="submit"></form>');
       return;
     }
     var sizePromise;
-    if (!videoSizes[videoId]) {
+    if (!videoSizes[videoId + itag]) {
       sizePromise = new Promise(function (resolve, reject) {
-        ytdl.downloadFromInfo(info, options).on('format', function (format) {
+        ytdl.downloadFromInfo(info, { quality: itag }).on('format', function (format) {
           if (format instanceof Error) {
             reject(error);
           } else {
-            videoSizes[videoId] = format.size;
+            videoSizes[videoId + itag] = format.size;
             resolve(format.size);
           }
         }).on('error', function () { reject('Not downloadable'); });
       });
     } else {
-      sizePromise = Promise.resolve(videoSizes[videoId]);
+      sizePromise = Promise.resolve(videoSizes[videoId + itag]);
     }
 
     sizePromise.then(function (size) {
+      var options = { quality: itag };
       var range = readRangeHeader(req.headers.range, size);
 
       if (range) {
@@ -103,18 +114,18 @@ function server(req, res) {
 
       ytdl.downloadFromInfo(info, options).on('format', function (format) {
         if (format instanceof Error) {
-          res.writeHead(403);
+          res.writeHead(404);
         } else if (!range) {
           res.writeHead(200, {
             'Content-Length': format.size,
-            'Content-Type': 'video/webm',
+            'Content-Type': format.type,
             'Accept-Ranges': 'bytes'
           });
         } else {
           res.writeHead(206, {
             'Content-Range': 'bytes ' + range.start + '-' + range.end + '/' + size,
             'Content-Length': range.start === range.end ? 0 : (range.end - range.start + 1),
-            'Content-Type': 'video/webm',
+            'Content-Type': format.type,
             'Accept-Ranges': 'bytes'
           });
         }
@@ -123,7 +134,7 @@ function server(req, res) {
       }).pipe(res);
     }).then(undefined, function (error) {
       res.writeHead(404);
-      res.end('Internal error ' + error);
+      res.end('Internal error: ' + error);
     });
   });
 }
