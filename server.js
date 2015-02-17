aa3947cbf3adb958bf66f96d72980bc64d1dce99
ebraminio <ebrahim@gnu.org>
2015-02-17 13:48:48 +0000

    Use resolution instead quality on ?info

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ function server(req, res) {
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end('<form method="GET"><select name="itag">' + info.formats.map(function (x) {
         return `<option value="${x.itag}">
-${x.container} ${x.quality || x.size || 'audio'} ${x.audioBitrate ? '' : 'no audio'}
+${x.container} ${x.resolution || 'audio'} ${x.audioBitrate ? '' : 'no audio'}
 </option>`;
       }).join('\n') + '</select><input type="submit"></form>');
       return;
